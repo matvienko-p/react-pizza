e9c739d5d8e479e89d3f048376854b3860fc17d1
Petr <matvienko.petya@gmail.com>
2023-03-03 11:51:01 +0500

    init: add react.js app and html-base

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './scss/app.scss';
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
+
+function App() {
+  return (
+    <div class='wrapper'>
+      <Header />
+      <div class='content'>
+        <div class='container'>
+          <div class='content__top'>
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class='content__title'>Все пиццы</h2>
+          <div class='content__items'>
+            <PizzaBlock title='Мексиканская' price={500} />
+            <PizzaBlock title='Пеликанская' price={600} />
+            <PizzaBlock title='Великанская' price={800} />
+            <PizzaBlock title='Американская' price={450} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
