8ab170f181285e6ceaa0d37b3ef908f5f4aa14fa
Petr <matvienko.petya@gmail.com>
2023-03-06 02:16:19 +0500

    feat: add json-file, active and clickable categoryies, types, sizes

--- src/App.js
+++ src/App.js
@@ -5,22 +5,28 @@ import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
 
+import pizzas from './assets/pizzas.json';
+
 function App() {
   return (
-    <div class='wrapper'>
+    <div className='wrapper'>
       <Header />
-      <div class='content'>
-        <div class='container'>
-          <div class='content__top'>
+      <div className='content'>
+        <div className='container'>
+          <div className='content__top'>
             <Categories />
             <Sort />
           </div>
-          <h2 class='content__title'>Все пиццы</h2>
-          <div class='content__items'>
-            <PizzaBlock title='Мексиканская' price={500} />
+          <h2 className='content__title'>Все пиццы</h2>
+          <div className='content__items'>
+            {pizzas.map((obj) => (
+              <PizzaBlock {...obj} />
+            ))}
+
+            {/* <PizzaBlock title='Мексиканская' price={500} />
             <PizzaBlock title='Пеликанская' price={600} />
             <PizzaBlock title='Великанская' price={800} />
-            <PizzaBlock title='Американская' price={450} />
+            <PizzaBlock title='Американская' price={450} /> */}
           </div>
         </div>
       </div>
